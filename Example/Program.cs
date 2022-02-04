a5c6c31bdde0ab166acefda3bcff10d2050341c1
sonodima <62671493+sonodima@users.noreply.github.com>
2022-02-04 15:12:34 -0800

    added nicer example

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,3 +1,23 @@
-﻿using Discover;
+﻿using System.Drawing;
 
-new Discover.Discover().Test();
+using Discover;
+using Example;
+
+var renderer = new Renderer();
+
+while (true)
+{
+    var mouse = User32.GetCursorPosition();
+
+    for (int i = -500; i <= 500; i += 100)
+    {
+        for (int j = -500; j <= 500; j += 100)
+        {
+            renderer.DrawString("Speed", "Arial", Color.Black, new Point(mouse.X + i, mouse.Y + j));
+        }
+    }
+
+    renderer.Present();
+
+    Thread.Sleep(6);
+}
---
+++ Example/User32.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace Example
+{
+    public static class User32
+    {
+        public struct WinPoint
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+
+            public static implicit operator Point(WinPoint point)
+            {
+                return new Point(point.X, point.Y);
+            }
+        }
+
+        [DllImport("user32.dll")]
+        private static extern bool GetCursorPos(out Point point);
+
+        public static Point GetCursorPosition()
+        {
+            GetCursorPos(out Point point);
+            return point;
+        }
+    }
+}
