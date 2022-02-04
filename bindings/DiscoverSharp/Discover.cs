aedda52168f6b517215ea63129540891453732df
sonodima <62671493+sonodima@users.noreply.github.com>
2022-02-04 03:17:38 -0800

    Revert "next update"

---
+++ bindings/DiscoverSharp/Discover.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace DiscoverSharp
+{
+    class Discover
+    {
+        [DllImport("discover.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        private static extern bool Start(int port);
+
+        [DllImport("discover.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        private static extern bool SubmitQueue();
+
+        [DllImport("discover.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        private static extern void DrawString(int x, int y, byte[] text, int color, float alpha,
+            bool stroke, int strokeColor, int strokeThickness);
+
+        [DllImport("discover.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        private static extern void DrawPolygon(int[] path, int color, float alpha, bool fill, int thickness);
+
+        [DllImport("discover.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        private static extern void DrawRectangle(int x1, int y1, int x2, int y2, int color,
+            float alpha, bool fill, int thickness, int radius);
+
+        [DllImport("discover.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        private static extern void DrawCircle(int x, int y, int radius, int color, float alpha,
+            bool fill, int thickness);
+
+        public int Port { get; }
+
+        public Discover(int port)
+        {
+            Port = port;
+        }
+
+        public bool Initialize()
+        {
+            return Start(Port);
+        }
+
+        public bool Patch(string address)
+        {
+            string local = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            string discordPath = Path.Combine(local, "Discord");
+
+            bool hasPatchedSomething = false;
+
+            string[] appDirectories = Directory.GetDirectories(discordPath, "app-*");
+            foreach (string appDirectory in appDirectories)
+            {
+                string overlayPath = Path.Combine(appDirectory, "modules\\discord_overlay2-1\\discord_overlay2");
+                string hostPath = Path.Combine(overlayPath, "host.js");
+                if (!Directory.Exists(overlayPath) || !File.Exists(hostPath))
+                {
+                    continue;
+                }
+
+                List<string> hostData = new(File.ReadAllLines(hostPath));
+
+                for (int i = 0; i < hostData.Count; i++)
+                {
+                    string currentLine = hostData[i];
+
+                    if (currentLine.Contains("url = urlWithPid.toString();"))
+                    {
+                        string patch = string.Format("url = \"http://{0}:{1}/\"", address, Port);
+
+                        if ((i + 1) < hostData.Count && hostData[i + 1].Contains("url = \"http://"))
+                        {
+                            // The file was already patched, update the next line.
+                            hostData[i + 1] = patch;
+                        }
+                        else
+                        {
+                            // The file is not patched, insert the patch.
+                            hostData.Insert(i + 1, patch);
+                        }
+
+                        hasPatchedSomething = true;
+                    }
+                }
+
+                File.WriteAllLines(hostPath, hostData);
+            }
+
+            return hasPatchedSomething;
+        }
+
+        private static int ConvertColor(Color color)
+        {
+            return (color.R << 16) + (color.G << 8) + color.B;
+        }
+
+#pragma warning disable CA1822 // Mark members as static
+        public bool Render()
+#pragma warning restore CA1822 // Mark members as static
+        {
+            return SubmitQueue();
+        }
+
+#pragma warning disable CA1822 // Mark members as static
+        public void PushString(Point position, string text, Color color)
+#pragma warning restore CA1822 // Mark members as static
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+            DrawString(position.X, position.Y, bytes, ConvertColor(color), color.A / 255.0f, false,
+                0x0, 0);
+        }
+
+#pragma warning disable CA1822 // Mark members as static
+        public void PushString(Point position, string text, Color color, Color strokeColor,
+             int strokeThickness)
+#pragma warning restore CA1822 // Mark members as static
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+            DrawString(position.X, position.Y, bytes, ConvertColor(color), color.A / 255.0f, true,
+                ConvertColor(strokeColor), strokeThickness);
+        }
+
+#pragma warning disable CA1822 // Mark members as static
+        public void PushRectangle(Rectangle rectangle, Color color, bool fill = true, int thickness = 0, int radius = 0)
+#pragma warning restore CA1822 // Mark members as static
+        {
+            DrawRectangle(rectangle.Left, rectangle.Top, rectangle.Right, rectangle.Bottom, ConvertColor(color),
+                color.A / 255.0f, fill, thickness, radius);
+        }
+
+#pragma warning disable CA1822 // Mark members as static
+        public void PushCircle(Point position, int radius, Color color, bool fill = true, int thickness = 0)
+#pragma warning restore CA1822 // Mark members as static
+        {
+            DrawCircle(position.X, position.Y, radius, ConvertColor(color), color.A / 255.0f, fill, thickness);
+        }
+
+#pragma warning disable CA1822 // Mark members as static
+        public void PushPolygon(Point[] points, Color color, bool fill = true, int thickness = 0)
+#pragma warning restore CA1822 // Mark members as static
+        {
+            int[] path = new int[points.Length * 2];
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                Point point = points[i];
+                path[i * 2] = point.X;
+                path[(i * 2) + 1] = point.Y;
+            }
+
+            DrawPolygon(path, ConvertColor(color), color.A / 255.0f, fill, thickness);
+        }
+
+    }
+}
---
+++ renderer/src/index.ts
@@ -0,0 +1,158 @@
+import * as PIXI from "pixi.js";
+import {
+  StringInstruction,
+  PolygonInstruction,
+  RectangleInstruction,
+  CircleInstruction,
+  RenderQueue,
+  InstructionType,
+} from "./types";
+
+document.body.style.overflow = "hidden";
+document.body.style.margin = "0px";
+
+const app = new PIXI.Application({
+  width: window.innerWidth,
+  height: window.innerHeight,
+  backgroundAlpha: 0,
+  antialias: true,
+});
+document.body.appendChild(app.view);
+
+const sleep = (ms: number) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+const drawString = (data: StringInstruction) => {
+  const style = new PIXI.TextStyle({
+    fill: data.color,
+  });
+
+  if (data.stroke) {
+    style.stroke = data.strokeColor;
+    style.strokeThickness = data.strokeThickness;
+  }
+
+  const child = new PIXI.Text(data.text, style);
+  child.alpha = data.alpha;
+  child.x = data.x;
+  child.y = data.y;
+  app.stage.addChild(child);
+};
+
+const drawPolygon = (graphics: PIXI.Graphics, data: PolygonInstruction) => {
+  if (data.fill) {
+    graphics.beginFill(data.color, data.alpha);
+    graphics.lineStyle(0);
+  } else if (data.thickness) {
+    graphics.lineStyle(data.thickness, data.color, data.alpha);
+  }
+
+  graphics.drawPolygon(data.path);
+
+  if (data.fill) {
+    graphics.endFill();
+  }
+};
+
+const drawRectangle = (graphics: PIXI.Graphics, data: RectangleInstruction) => {
+  if (data.fill) {
+    graphics.beginFill(data.color, data.alpha);
+    graphics.lineStyle(0);
+  } else if (data.thickness) {
+    graphics.lineStyle(data.thickness, data.color, data.alpha);
+  }
+
+  if (data.radius) {
+    graphics.drawRoundedRect(
+      data.x1,
+      data.y2,
+      data.x2 - data.x1,
+      data.y2 - data.y1,
+      data.radius
+    );
+  } else {
+    graphics.drawRect(data.x1, data.y2, data.x2 - data.x1, data.y2 - data.y1);
+  }
+
+  if (data.fill) {
+    graphics.endFill();
+  }
+};
+
+const drawCircle = (graphics: PIXI.Graphics, data: CircleInstruction) => {
+  if (data.fill) {
+    graphics.beginFill(data.color, data.alpha);
+    graphics.lineStyle(0);
+  } else if (data.thickness) {
+    graphics.lineStyle(data.thickness, data.color, data.alpha);
+  }
+
+  graphics.drawCircle(data.x, data.y, data.radius);
+
+  if (data.fill) {
+    graphics.endFill();
+  }
+};
+
+let socket: WebSocket | undefined;
+
+const connect = async () => {
+  await new Promise((resolve) => {
+    socket = new WebSocket("ws://localhost:4001/ws/queue");
+
+    socket.addEventListener("open", (event) => {
+      console.log("opened");
+      resolve(true);
+    });
+
+    socket.addEventListener("close", async () => {
+      app.stage.removeChildren();
+      socket = undefined;
+      await sleep(1000);
+      connect();
+      resolve(false);
+    });
+
+    socket.addEventListener("error", async () => {
+      app.stage.removeChildren();
+      socket = undefined;
+      await sleep(1000);
+      connect();
+      resolve(false);
+    });
+
+    socket.addEventListener("message", (event: MessageEvent<string>) => {
+      const queue: RenderQueue = JSON.parse(event.data);
+      app.stage.removeChildren();
+
+      const graphics = new PIXI.Graphics();
+
+      queue.instructions.forEach((instruction) => {
+        switch (instruction.type) {
+          case InstructionType.String:
+            drawString(instruction.data);
+            break;
+
+          case InstructionType.Polygon:
+            drawPolygon(graphics, instruction.data);
+            break;
+
+          case InstructionType.Rectangle:
+            drawRectangle(graphics, instruction.data);
+            break;
+
+          case InstructionType.Circle:
+            drawCircle(graphics, instruction.data);
+
+          default:
+            break;
+        }
+      });
+
+      app.stage.addChild(graphics);
+    });
+  });
+};
+
+connect();
